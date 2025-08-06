5c55dbf75eb2a745e2fe737e710de1ed2fff8f13
Matheus Richard <matheusperpetuo@gmail.com>
2025-08-06 19:43:10 -0300

    organize components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
 import './styles/global.css'
 import './styles/theme.css'
 
+
+import { Heading } from './components/Heading'
+import { Container } from './components/Container'
+
 export function App () {
 
+    return (
+         <>
+        <Container>
+            <Heading>LOGO</Heading>
+        </Container>
 
-    return ( <>
-        <div className='container'>
-            <div className='content'>
-                <section>LOGO</section>
-            </div>
-        </div>
-
-      <div className='container'>
-            <div className='content'>
-                <section>MENU</section>
-            </div>
-        </div>
-
-      <div className='container'>
-            <div className='content'>
-                <section>FORM</section>
-            </div>
-        </div>
-
-      <div className='container'>
-            <div className='content'>
-                <section>FOOTER</section>
-            </div>
-        </div>
+        <Container>
+            <Heading>MENU</Heading>
+        </Container>
         </>
     )
 }
