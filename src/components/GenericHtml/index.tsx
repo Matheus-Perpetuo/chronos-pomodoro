bb31ed73d6a4e5662ca3b160912dedf388a89a6c
Matheus Richard <matheusperpetuo@gmail.com>
2025-08-12 21:50:14 -0300

    Create pages and Templates

---
+++ src/components/GenericHtml/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from 'react';
+import styles from './styles.module.css'
+
+type GenericHtmlProps = {
+    children: ReactNode;
+};
+
+export function GenericHtml({children}: GenericHtmlProps) {
+    return <div className={styles.genericHtml}>{children}</div>;
+}
