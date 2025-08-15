86a2f024f3c2376bca2c13d64a8e1add667ccf13
Richard <matheusperpetuo@gmail.com>
2025-08-15 15:39:26 -0300

    Add getNextCycle

--- src/contexts/TaskContext/inititalTaskState.ts
+++ src/contexts/TaskContext/inititalTaskState.ts
@@ -5,9 +5,9 @@ export const initialTasksState: TaskStateModel = {
     secondsRemaining: 0,
     formattedSecondsRemaining: '00:00',
     activeTask: null, 
-    currentCycle: 0, 
+    currentCycle: 0, // 1, 2,3 ... 8 => 1
     config: {
-        worktime: 25,
+        workTime: 25,
         shortBreakTime: 5,
         longBreakTime: 15,
     },
