c08a988b415e326c54120328e0249d730a550006
Richard <matheusperpetuo@gmail.com>
2025-08-21 17:17:40 -0300

    Create timerWorkes

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { initialTasksState } from "./inititalTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workes/TimerWorkerManager";
 
  type TaskContextProviderProps = {
     children: React.ReactNode;
@@ -10,9 +11,26 @@ import { taskReducer } from "./taskReducer";
  export function TaskContextProvider({children}: TaskContextProviderProps) {
     const [state, dispatch] = useReducer(taskReducer, initialTasksState);
 
+    const worker = TimerWorkerManager.getInstance()
+
+    worker.onmessage(e => {
+      const countDownSeconds = e.data
+      console.log(countDownSeconds);
+      
+      if(countDownSeconds <= 0){
+         console.log('Worker COMPLETED')
+         worker.terminate();
+      };
+    });
+
    useEffect(()=>{
-      console.log(state);
-   },[state])
+      if(!state.activeTask){
+         console.log('Worker Terminado por falta de activeTask')
+         worker.terminate();
+      }
+
+      worker.postMessage(state);
+   }, [worker, state]);
 
    return <TaskContext.Provider value={{ state, dispatch }}>
       {children}
