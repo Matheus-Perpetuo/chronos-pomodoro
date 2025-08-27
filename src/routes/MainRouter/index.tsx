2350b7b7e89aae392f07ca20737dcb1624ae45be
Matheus Richard <matheusperpetuo@gmail.com>
2025-08-27 20:40:59 -0300

    Config settings page

--- src/routes/MainRouter/index.tsx
+++ src/routes/MainRouter/index.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "../../pages/NotFound";
 import { AboutPomodoro } from "../../pages/AboutPomodoro";
 import { useEffect } from "react";
 import { History } from "../../pages/History";
+import { Settings } from "../../pages/Settings";
 
 function ScrollToTop() {
     const {pathname} = useLocation();
@@ -20,8 +21,9 @@ export function MainRouter() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />}/>
-                <Route path='/abou-pomodoro/' element={<AboutPomodoro />}/>
                 <Route path='/history/' element={<History />}/>
+                <Route path='/settings/' element={<Settings />}/>
+                <Route path='/abou-pomodoro/' element={<AboutPomodoro />}/>
                 <Route path='*' element={<NotFound />}/>
             </Routes>
             <ScrollToTop />
